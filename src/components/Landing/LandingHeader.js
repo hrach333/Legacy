87b8a8da05f9f40655bc0dd7f23b318ec3d4eb5e
Talla2XLC <mmm-appp@mail.ru>
2020-04-14 23:49:05 +0300

    add header
    new features:
    -support sass dependencie
    -folder architecture upd
    -upload header svg

---
+++ src/components/Landing/LandingHeader.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './LandingHeader.sass';
+import { ReactComponent as HeaderSVG } from './svg/header.svg';
+import { ReactComponent as HeaderArrowSVG } from './svg/header_arrow.svg';
+
+function LandingHeader() {
+  return (
+    <div className="landing-header">
+    	<div className="header-content">
+        <h3>Сервис</h3>
+        <h1>Наследие</h1>
+        <span>Создаем цифровой семейный <br/>архив</span>
+        <HeaderArrowSVG />
+    	</div>
+    	<div className="headerSVG-div">
+        <HeaderSVG />
+      </div>
+    </div>
+  );
+}
+
+export default LandingHeader;
---
+++ src/components/Landing/LandingMission.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './LandingMission.css';
+
+function LandingMission() {
+  return (
+    <div className="landing-mission">
+    </div>
+  );
+}
+
+export default LandingMission;
