d8f92e3e1874798b09bbf3c7b9dbf35d4a07ad1f
Talla2XLC <mmm-appp@mail.ru>
2020-04-14 18:52:35 +0300

    tic tac toe start

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Начнём по-тихоньку с проектом "Наследие"
+          Начнём потихоньку с проектом "Наследие"
         </p>
       </header>
     </div>
