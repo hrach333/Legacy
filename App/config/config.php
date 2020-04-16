6a4d3340058e6020be196aa214ea8d7fd5812115
hrach333 <hrach@hrach.ru>
2020-04-16 09:56:13 +0300

    добавлен конфигурационый файл

---
+++ App/config/config.php
@@ -0,0 +1,18 @@
+<?php
+
+$config = [
+    'prod' => [
+        'conect_db' => [
+            
+        ]
+    ],
+    'dev' => [
+        'conect_db' =>[
+            'host' =>'localhost',
+            'db' => 'legacy',
+            'user_db' => 'root',
+            'pass_db' => 'mpmegmrx'
+        ],
+        
+    ]
+];
